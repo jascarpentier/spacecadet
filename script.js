99d8c704d62ebd1e03d8e3cb2c301739bce16a8c
jasmine carpentier <jascarpentier@gmail.com>
2019-06-10 14:24:14 -0400

    javascript step one

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-const button = document.querySelector('button');
+const DOMAIN = 'https://www.googleapis.com/books/v1/volumes?q=search+terms';
 const API_KEY = 'AIzaSyCMRkIUhX9QITmY8RIsacHdHSCB_DM_jLA';
 const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`;
-const DOMAIN = '';
+const button = document.querySelector('button');
 const list = document.querySelector('list');
 const collectionName = document.querySelector('collectionName');
+const bookList = document.querySelector('bookList');
+
+
+const render = (books) => {
+  books.forEach(book => {
+    const el = document.createElement('div');
+    console.log(el)
+
+    el.innerHTML = `
+          <h2>${book.volumeInfo.title}</h2>
+          <p>${book.volumeInfo.authors}</p>
+          <p>${book.searchInfo.textSnippet}</p>
+        `;
+    document.body.appendChild(el)
+  });
+
+}
+
+
+
 
 button.addEventListener('click', async () => {
-  const searchValue = document.querySelectorI('input').value;
-  const response = await axios.get('https://www.googleapis.com/books/v1/${collectionName}/resourceID?parameters')
-
+  const searchTerms = document.querySelector('input').value;
+  const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchTerms}`)
+  const books = response.data.items;
+  console.log(books)
+  render(books)
 })
+
